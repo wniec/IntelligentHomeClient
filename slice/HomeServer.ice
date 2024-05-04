a76955f1cf8476f4cd817632c02b77abb992bb20
wniec <niecwladek@gmail.com>
2024-05-04 16:54:11 +0200

    Initial commit

---
+++ slice/HomeServer.ice
@@ -0,0 +1,50 @@
+
+#ifndef HOME_ICE
+#define HOME_ICE
+
+module generated
+{
+  
+  exception NoInput {};
+  dictionary <string, int> contents;
+
+  interface Camera
+  {
+  void move(double phi, double psi);
+  void zoomIn(double value);
+  void zoomOut(double value);
+  bool moveDetected();
+  };
+  interface CentralHeating
+  {
+  void turnOn();
+  void turnOff();
+  bool isOn();
+  double getTemperature();
+  void setTemperature(double temperature);
+  };
+  interface Fridge
+  {
+  void put(string product, int amount);
+  void take(string product, int amount);
+  contents list();
+  void setTemperature(double temperature);
+  };
+  interface GardenCamera extends Camera{
+  void turnInfraredOn();
+  void turnInfraredOff();
+  };
+  interface Lamp{
+  void turnOn();
+  void turnOff();
+  bool isOn();
+  void setBrightness(int lumens);
+  };
+  interface Garage{
+  void open();
+  void close();
+  bool isOpen();
+  };
+};
+
+#endif
---
+++ test.py
@@ -0,0 +1,46 @@
+import sys
+
+import Ice
+import generated
+
+
+def main():
+    with Ice.initialize(sys.argv) as communicator:
+        server1 = "tcp -h 127.0.0.2 -p 10000 -z : udp -h 127.0.0.2 -p 10000 -z"
+        server2 = "tcp -h 127.0.0.3 -p 10000 -z : udp -h 127.0.0.3 -p 10000 -z"
+        for i, server in enumerate((server1, server2)):
+            print(f"SERVER {i}")
+            camera = communicator.stringToProxy("camera/camera:"+server)
+            central_heating = communicator.stringToProxy("heating/heating:"+server)
+            fridge = communicator.stringToProxy("fridge/fridge:"+server)
+            garage = communicator.stringToProxy("garage/garage:"+server)
+            garden_camera = communicator.stringToProxy("garden/garden:"+server)
+            lamp_4 = communicator.stringToProxy("lamp/4:"+server)
+
+            camera = generated.CameraPrx.checkedCast(camera)
+            central_heating = generated.CentralHeatingPrx.checkedCast(central_heating)
+            fridge = generated.FridgePrx.checkedCast(fridge)
+            garage = generated.GaragePrx.checkedCast(garage)
+            garden_camera = generated.GardenCameraPrx.checkedCast(garden_camera)
+            lamp_4 = generated.LampPrx.checkedCast(lamp_4)
+
+            if not camera or not central_heating or not fridge or not garage or not garden_camera or not lamp_4:
+                raise RuntimeError("Invalid proxy")
+
+            print(camera.moveDetected())
+            fridge.put("CHEESE", 5)
+            fridge.put("MILK", 2)
+            print(fridge.list())
+            print(central_heating.getTemperature())
+            garage.open()
+            garage.close()
+            print(garage.isOpen())
+            garden_camera.turnInfraredOn()
+            garden_camera.turnInfraredOff()
+            lamp_4.turnOff()
+            lamp_4.turnOn()
+            lamp_4.isOn()
+
+
+if __name__ == '__main__':
+    main()
